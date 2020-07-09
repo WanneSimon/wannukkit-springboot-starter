33fe3938b79fd7195f6ee18e2183b17d99576d55
wanne <wanneseville@foxmail.com>
2020-07-09 14:03:41 +0800

    plguinLoader#PluginDescription

--- src/main/java/cc/wanforme/nukkit/loader/ExtPluginLoader.java
+++ src/main/java/cc/wanforme/nukkit/loader/ExtPluginLoader.java
@@ -1,27 +1,31 @@
 package cc.wanforme.nukkit.loader;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.regex.Pattern;
 
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginDescription;
 import cn.nukkit.plugin.PluginLoader;
+import cn.nukkit.utils.Utils;
 
 /**
  * @author wanne
  * 2020年7月3日
  */
 public class ExtPluginLoader implements PluginLoader{
-
 	// 
 	private final Map<String, Class> globalClasses = new HashMap<>();
 	
 	@Override
 	public Plugin loadPlugin(String filename) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return this.loadPlugin(new File(filename));
 	}
 
 	@Override
@@ -32,20 +36,51 @@ public class ExtPluginLoader implements PluginLoader{
 
 	@Override
 	public PluginDescription getPluginDescription(String filename) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.getPluginDescription(new File(filename));
 	}
 
 	@Override
 	public PluginDescription getPluginDescription(File file) {
-		// TODO Auto-generated method stub
-		return null;
+		// jar 类型
+		if(file.getName().endsWith(".jar")) {
+	        try (JarFile jar = new JarFile(file)) {
+	            JarEntry entry = jar.getJarEntry("nukkit.yml");
+	            if (entry == null) {
+	                entry = jar.getJarEntry("plugin.yml");
+	                if (entry == null) {
+	                    return null;
+	                }
+	            }
+	            try (InputStream stream = jar.getInputStream(entry)) {
+	                return new PluginDescription(Utils.readFile(stream));
+	            }
+	        } catch (IOException e) {
+	            return null;
+	        }
+		} else {
+			// 文件夹类型
+			File desFile = new File(file.getAbsolutePath()+"/nukkit.yml");
+			if(!desFile.exists()) {
+				desFile = new File(file.getAbsolutePath()+"/plugin.yml");
+			}
+			
+			if(!desFile.exists()) {
+				return null;
+			}
+			
+			try (InputStream stream = new FileInputStream(desFile)) {
+                return new PluginDescription(Utils.readFile(stream));
+            } catch (IOException e) {
+	            return null;
+	        }
+		}
+		
 	}
 
+	// jar文件和文件夹（当然也包括没有后缀的文件）
 	@Override
 	public Pattern[] getPluginFilters() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Pattern[] {Pattern.compile("^.+")};
 	}
 
 	@Override
@@ -68,4 +103,10 @@ public class ExtPluginLoader implements PluginLoader{
 	Class<?> getGlobalClassByName(String name) {
 		return globalClasses.get(name);
 	}
+	
+	/** 判断是否是文件夹 或 jar文件*/
+	protected boolean isJarOrDirectory(File file) {
+		return file.isDirectory() || file.getName().endsWith(".jar");
+	}
+	
 }
